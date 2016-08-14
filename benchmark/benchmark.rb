5587151b7c7325d2cddc718481d653d5e8649732
Takashi Kokubun <takashikkbn@gmail.com>
2016-08-15 00:37:19 +0900

    Add benchmark

---
+++ benchmark/benchmark.rb
@@ -0,0 +1,21 @@
+require 'benchmark/ips'
+require 'escape_utils'
+require 'hescape'
+require 'cgi/escape' # Require Ruby 2.3+
+
+Benchmark.ips do |x|
+  str = ENV['ESCAPE_STRING'] || "https://github.com/k0kubun/hescape-ruby"
+  escape_table = {
+    "'" => '&#39;',
+    '&' => '&amp;',
+    '"' => '&quot;',
+    '<' => '&lt;',
+    '>' => '&gt;',
+  }
+
+  x.report('gsub')         { str.gsub(/['&\"<>]/, escape_table) }
+  x.report('escape_utils') { EscapeUtils.escape_html(str, false) }
+  x.report('cgi/escape')   { CGI.escapeHTML(str) }
+  x.report('hescape')      { Hescape.escape_html(str) }
+  x.compare!
+end
