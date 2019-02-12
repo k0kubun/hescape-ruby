073f12db716e1ac99d62b61d6d6313ca15651e19
Takashi Kokubun <takashikkbn@gmail.com>
2019-02-12 23:47:53 +0900

    Fix escaping for index-changing cases
    Close #2

--- spec/hescape_spec.rb
+++ spec/hescape_spec.rb
@@ -14,5 +14,9 @@ RSpec.describe Hescape do
       expect(escape_html(%[<a href="foo=1&bar=2" target='_blank'>#</a>]))
         .to eq('&lt;a href=&quot;foo=1&amp;bar=2&quot; target=&#39;_blank&#39;&gt;#&lt;/a&gt;')
     end
+
+    it 'escapes double quotes properly on an edge case' do
+      expect(escape_html(%[ ""               ])).to eq(%[ &quot;&quot;               ])
+    end
   end
 end
