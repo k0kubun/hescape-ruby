86ccb569c717e555f1545cbbf20ab592029450d4
Takashi Kokubun <takashikkbn@gmail.com>
2019-02-12 23:46:57 +0900

    Cosmetic changes for specs

--- spec/hescape_spec.rb
+++ spec/hescape_spec.rb
@@ -6,10 +6,13 @@ RSpec.describe Hescape do
   end
 
   describe '.escape_html' do
-    it { expect { escape_html(nil) }.to raise_error TypeError, /expected String/ }
-    it 'should escape special characters' do
-      expect(escape_html(%(<a href="foo=1&bar=2" target='_blank'>#</a>)))
-        .to eq '&lt;a href=&quot;foo=1&amp;bar=2&quot; target=&#39;_blank&#39;&gt;#&lt;/a&gt;'
+    it 'raises ArgumentError given nil' do
+      expect { escape_html(nil) }.to raise_error(TypeError, /expected String/)
+    end
+
+    it 'escapes special characters' do
+      expect(escape_html(%[<a href="foo=1&bar=2" target='_blank'>#</a>]))
+        .to eq('&lt;a href=&quot;foo=1&amp;bar=2&quot; target=&#39;_blank&#39;&gt;#&lt;/a&gt;')
     end
   end
 end
